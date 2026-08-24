1b7e8e90b5bc5126873f084e73c1ce08207d844a
yashwanth-1800 <munugurayash2008@gmail.com>
2026-08-24 23:20:22 +0530

    Remove Firebase prototype file: firebase-config.js

--- firebase-config.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyD7NMp66LLaZYi_5uqbrbU-SFCJRCRyTmY",
-  authDomain: "emergency-app-f2850.firebaseapp.com",
-  projectId: "emergency-app-f2850",
-  storageBucket: "emergency-app-f2850.firebasestorage.app",
-  messagingSenderId: "206775317622",
-  appId: "1:206775317622:web:8b051ba2dfe92858ca1b57"
-};
-
